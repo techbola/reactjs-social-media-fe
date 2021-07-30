20359c9699f99c420de1d4088c784acd07a84ca0
Adebola <adebola@rb2.ng>
2021-07-30 01:06:11 +0100

    redux setup, post reducer and get all posts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
+
 import memories from "./images/memories.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
@@ -7,6 +11,11 @@ import useStyles from "./styles";
 
 function App() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxWidth="lg">
